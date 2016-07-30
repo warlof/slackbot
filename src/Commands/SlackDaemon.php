b2519a29d6230370491162553c51c96df0b1ebbd
elfaus <elfaus@hotmail.fr>
2016-07-30 14:23:13 +0200

    replace the daemon api with a built in one fix issue which prevent usage of stable flag

--- src/Commands/SlackDaemon.php
+++ src/Commands/SlackDaemon.php
@@ -9,12 +9,15 @@ namespace Seat\Slackbot\Commands;
 
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Log;
-use PhpSlackBot\Bot;
+use Ratchet\Client\Connector;
+use Ratchet\Client\WebSocket;
+use Ratchet\RFC6455\Messaging\MessageInterface;
+use React\EventLoop\Factory;
 use Seat\Services\Settings\Seat;
-use Seat\Slackbot\Events\SlackEventHandler;
 use Seat\Slackbot\Exceptions\SlackSettingException;
 use Seat\Slackbot\Helpers\SlackApi;
+use Seat\Slackbot\Models\SlackChannel;
+use Seat\Slackbot\Models\SlackUser;
 
 class SlackDaemon extends Command
 {
@@ -33,58 +36,92 @@ class SlackDaemon extends Command
 
         if ($token == null)
             throw new SlackSettingException("missing slack_token in settings");
-        
+
         // call rtm method in order to get a fresh new WSS uri
         $api = new SlackApi($token);
         $wss = $api->rtmStart();
 
-        // use the Web Socket Service uri in order to connect to the Slack team
-        \Ratchet\Client\connect($wss)->then(function($conn){
-            $debug = false;
-            $conn->on('message', function($msg) use ($conn, $debug){
-                if ($debug == false) {
-                    $dump = print_r($msg, true);
-                    Log::debug($dump);
-                    $debug = true;
-                    echo "done";
+        // start a loop event which will handle RTM daemon
+        $loop = Factory::create();
+        $connector = new Connector($loop);
+
+        // prepare the event catcher (we only care about members join and channels)
+        $connector($wss)->then(function(WebSocket $conn) {
+            // trigger on RTM message event
+            $conn->on('message', function(MessageInterface $msg) use ($conn){
+                // since Slack RTM return json message, decode it first
+                $slackMessage = json_decode($msg, true);
+
+                // then, process to channel, groups and member case
+                switch ($slackMessage['type']) {
+                    // if the event is of type "team_join", then update our Slack user table using the new slack user id
+                    // common element between SeAT and Slack is mail address
+                    case 'team_join':
+                        $slackUser = SlackUser::join('users', 'users.id', 'slack_users.user_id')
+                            ->where('email', $slackMessage['user']['profile']['email'])
+                            ->first();
+
+                        if ($slackUser != null) {
+                            $slackUser->update(['slack_id' => $slackMessage['user']['id']]);
+                        }
+
+                        break;
+
+                    // if the event is of type "channel_created", then update our Slack channel table using new slack channel id
+                    case 'group_joined':
+                    case 'channel_created':
+                        $channel = SlackChannel::find($slackMessage['channel']);
+
+                        if ($channel->count() == 0) {
+                            $channel = new SlackChannel();
+                            $channel->id = $slackMessage['channel']['id'];
+                            $channel->name = $slackMessage['channel']['name'];
+
+                            if ($slackMessage['type'] == 'channel_created') {
+                                $channel->is_group = false;
+                            } else {
+                                $channel->is_group = true;
+                            }
+
+                            $channel->save();
+                        }
+
+                        break;
+                    // if the event is of type "channel_delete", then remove the record from our Slack channel table
+                    case 'channel_deleted':
+                    case 'group_archive':
+                        $channel = SlackChannel::find($slackMessage['channel']);
+
+                        if ($channel->count() != 0)
+                            $channel->delete();
+
+                        break;
+                    case 'group_unarchive':
+                        // load token and team uri from settings
+                        $token = Seat::get('slack_token');
+
+                        if ($token == null)
+                            throw new SlackSettingException("missing slack_token in settings");
+
+                        $slackApi = new SlackApi($token);
+                        $apiGroup = $slackApi->info($slackMessage['channel'], true);
+
+                        $group = new SlackChannel();
+                        $group->id = $apiGroup['id'];
+                        $group->name = $apiGroup['name'];
+                        $group->is_group = true;
+                        $group->save();
+
+                        break;
                 }
             });
-/*
-            $conn->on('team_join', function($msg) use ($conn){
-                print_r('team_join');
-            });
-
-            $conn->on('group_joined', function($msg) use ($conn){
-                print_r('group_joined');
-            });
-
-            $conn->on('channel_created', function($msg) use ($conn){
-                print_r('channel_created');
-            });
-
-            $conn->on('channel_deleted', function($msg) use ($conn){
-                print_r('channel_deleted');
-            });
-
-            $conn->on('group_archive', function($msg) use ($conn){
-                Log::info($msg);
-                Log::info('group archived');
-            });
-
-            $conn->on('group_unarchive', function($msg) use ($conn){
-                print_r('group_unarchive');
-            });
-*/
+        },
+        function(\Exception $e) use ($loop) {
+            echo $e->getMessage();
+            $loop->stop();
         });
 
-        /*
-        $bot = new Bot();
-        $bot->setToken($token);
-
-        // catch all event and return them to SlackTeamJoin
-        $bot->loadCatchAllCommand(new SlackEventHandler());
-        $bot->run();
-        */
+        $loop->run();
 
         return;
     }
