894638301d4ca0c182a059f8c88f4ba8a6d7eb67
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-02-03 13:51:29 +0100

    chore(license): Update license header to cover 2019

--- src/lang/af/seat.php
+++ src/lang/af/seat.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of slackbot and provide user synchronization between both SeAT and a Slack Team
  *
- * Copyright (C) 2016, 2017, 2018  Loïc Leuilliot <loic.leuilliot@gmail.com>
+ * Copyright (C) 2016, 2017, 2018, 2019  Loïc Leuilliot <loic.leuilliot@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
